3a9dc04699a7989c90235893549d6b8b9d7c9074
Ruslan Absalyamov <ruslan.absalyamov@gmail.com>
2016-06-08 00:11:39 +0300

    Fixed indention by grouped xml nodes.

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,13 @@ extern crate xml;
 use std::fs::File;
 use std::io::BufReader;
 use std::str;
+use std::io::Write;
 
 use xml::reader::EventReader;
 use xml::writer::EmitterConfig;
 use xml::reader::XmlEvent;
-
+use xml::writer::EventWriter;
+use xml::writer::events::XmlEvent as WriterXmlEvent;
 
 fn main() {
     let file_in = File::open("..\\docs\\file.xml").unwrap();
@@ -19,26 +21,36 @@ fn main() {
     for ev in reader {
         match ev {
             Ok(XmlEvent::Whitespace(ref data)) => {
-                /*println!("Whitespace: {:?}", data);
-
-                if "\r\n\r\n" == data {
-                    match writer.write(xml::writer::events::XmlEvent::Characters(data)) {
-                        Ok(_) => {}
-
-                        Err(ev) => panic!("Writer error: {:?}", ev),
-                    }
-                }*/
+                // a little bit hacky)
+                if new_line_count(data) >= 2 {
+                    writer.write_safe(WriterXmlEvent::Characters("\r\n\r\n\t"));
+                }
             }
             Ok(ev) => {
                 if let Some(ev) = ev.as_writer_event() {
-                    match writer.write(ev) {
-                        Ok(_) => {}
-
-                        Err(ev) => panic!("Writer error: {:?}", ev),
-                    }
+                    writer.write_safe(ev);
                 }
             }
             Err(ev) => panic!("Error: {}", ev),
+        }
+    }
+}
+
+fn new_line_count(st: &str) -> i32 {
+    st.chars().filter(|&x| x == '\n').count() as i32
+}
+
+trait SafeWrite {
+    fn write_safe(&mut self, event: WriterXmlEvent);
+}
+
+
+impl<W: Write> SafeWrite for EventWriter<W> {
+    fn write_safe(&mut self, event: WriterXmlEvent) {
+        match self.write(event) {
+            Ok(_) => {}
+
+            Err(ev) => panic!("Writer error: {:?}", ev),
         }
     }
 }
