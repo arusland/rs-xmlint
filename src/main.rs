7ee31a6db2f7086a36ec88df8063417072b112d7
Ruslan Absalyamov <ruslan.absalyamov@gmail.com>
2016-07-02 20:36:19 +0300

    Supported non-UTF8 files.

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,9 @@ pub mod util;
 use std::fs::File;
 use std::io::BufReader;
 use std::io::BufRead;
-use std::str::*;
+use std::fs;
+use std::io::{Read, Write};
+use std::env;
 
 use xml::reader::EventReader;
 use xml::writer::EmitterConfig;
@@ -20,19 +22,37 @@ use util::{SafeWrite, new_line_count};
 
 
 fn main() {
-    let file_in = File::open("d:\\WORK\\MyProjects\\rust\\rs-xmlint\\docs\\file_1251.xml").unwrap();    
-    let mut file_out = File::create("..\\file_result.xml").unwrap();
-    let encoding = detect_encoding(&file_in);
+	let args: Vec<_> = env::args().collect();
 
-    println!("encoding: {}", encoding.expect("cannot detect encoding").name());
-    // TODO: detect source encoding
-    // convert to utf8
-    // format xml
-    // convert to source encoding
-    // save result xml
+    if args.len() <= 2 {
+        println!("USAGE: xmlint file.xml file_result.xml");
+        return;
+    }
 
-    let reader = EventReader::new(BufReader::new(&file_in));
-    let mut writer = EmitterConfig::default().perform_indent(true).create_writer(&mut file_out);
+	let file_name = &args[1];
+    let file_name_target = &args[2];
+    let mut file_name_result = file_name_target.to_string();
+    let encoding = detect_encoding(&file_name).expect("cannot detect encoding");
+    let mut convert = false;
+    let str: String; 
+
+    println!("Detected encoding: {}", encoding.name());
+   
+    let read : Box<Read> = match encoding.name() {
+    	"utf-8" => {	
+    		Box::new(BufReader::new(File::open(file_name).unwrap()))
+    	}
+    	_ => {
+    		convert = true;
+    		file_name_result.push_str(".tmp");
+		    str = decode(file_name, &encoding);
+		    Box::new(str.as_bytes())
+    	}
+    };
+
+    let reader = EventReader::new(read);
+    let mut file_result = File::create(&file_name_result).unwrap();
+    let mut writer = EmitterConfig::default().perform_indent(true).create_writer(&mut file_result);
 
     for ev in reader {
         match ev {
@@ -50,13 +70,21 @@ fn main() {
             Err(ev) => panic!("Error: {:?}", ev),
         }
     }
+
+    if convert {
+    	encode(&file_name_result, &file_name_target, &encoding);
+    	fs::remove_file(file_name_result).expect("Deleting file failed");
+    }
+
+    println!("File converted to {}", file_name_target);
 }
 
-fn detect_encoding(file: &File) -> Option<EncodingRef> {
+fn detect_encoding(file_name: &str) -> Option<EncodingRef> {
     use regex::Regex;
     use encoding::label::encoding_from_whatwg_label;
 
     let re = Regex::new("encoding\\s*=\\s*['\"]([^'\"]+)['\"]").unwrap();
+    let file = File::open(file_name).unwrap();    
     let file = BufReader::new(file);
 
     // check only first line
@@ -69,4 +97,42 @@ fn detect_encoding(file: &File) -> Option<EncodingRef> {
     }
 
     Some(UTF_8 as EncodingRef)
+}
+
+/// decode file into utf8 file
+fn decode(file_name: &str, inenc: &EncodingRef) -> String {
+	use encoding::{DecoderTrap};
+
+	println!("Decoding({}) {}", inenc.name(), file_name);
+	let mut file = File::open(file_name).unwrap();
+	let mut ret = Vec::new();
+	let ll = file.read_to_end(&mut ret);
+
+	println!("File size {}", ll.unwrap());
+
+	let decoded = match inenc.decode(&ret, DecoderTrap::Strict) {
+        Ok(s) => s,
+        Err(e) => panic!("decoder error: {}", e),
+    };
+
+    decoded
+}
+
+// encode file into specified encoding
+fn encode(file_name: &str, file_name_result: &str, outenc: &EncodingRef) {
+	use encoding::{EncoderTrap};
+
+	println!("Encoding ({}) {}", outenc.name(), file_name_result);
+
+	let mut file_in = File::open(file_name).unwrap();
+	let mut content = String::new();
+	file_in.read_to_string(&mut content).unwrap();
+	let mut file_out = File::create(file_name_result).unwrap();
+
+	let encoded = match outenc.encode(&content, EncoderTrap::Strict) {
+        Ok(s) => s,
+        Err(e) => panic!("encoder error: {}", e),
+    };
+
+    file_out.write_all(&encoded).expect(&format!("File write failed: {}", file_name_result));
 }
